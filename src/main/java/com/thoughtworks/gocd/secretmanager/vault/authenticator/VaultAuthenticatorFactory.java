4fab60c3b1b979b31b8c5a33814723f26599b90b
Andrea Stacchiotti <andrea.stacchiotti@acus.eu>
2023-12-11 05:32:21 +0100

    Mild factory cleanup

--- src/main/java/com/thoughtworks/gocd/secretmanager/vault/authenticator/VaultAuthenticatorFactory.java
+++ src/main/java/com/thoughtworks/gocd/secretmanager/vault/authenticator/VaultAuthenticatorFactory.java
@@ -18,17 +18,14 @@ package com.thoughtworks.gocd.secretmanager.vault.authenticator;
 
 import com.thoughtworks.gocd.secretmanager.vault.models.SecretConfig;
 
-import java.util.HashMap;
 import java.util.Map;
 
 public class VaultAuthenticatorFactory {
-    private static Map<String, VaultAuthenticator> authMethodToAuthenticator = new HashMap<>();
-
-    static {
-        authMethodToAuthenticator.put(SecretConfig.TOKEN_AUTH_METHOD, new TokenAuthenticator());
-        authMethodToAuthenticator.put(SecretConfig.APPROLE_AUTH_METHOD, new AppRoleAuthenticator());
-        authMethodToAuthenticator.put(SecretConfig.CERT_AUTH_METHOD, new CertAuthenticator());
-    }
+    private static final Map<String, VaultAuthenticator> authMethodToAuthenticator = Map.of(
+            SecretConfig.TOKEN_AUTH_METHOD, new TokenAuthenticator(),
+            SecretConfig.APPROLE_AUTH_METHOD, new AppRoleAuthenticator(),
+            SecretConfig.CERT_AUTH_METHOD, new CertAuthenticator()
+    );
 
     public VaultAuthenticator authenticatorFor(SecretConfig secretConfig) {
         return authMethodToAuthenticator.get(secretConfig.getAuthMethod());
--- src/main/java/com/thoughtworks/gocd/secretmanager/vault/builders/VaultConfigBuilderFactory.java
+++ src/main/java/com/thoughtworks/gocd/secretmanager/vault/builders/VaultConfigBuilderFactory.java
@@ -18,17 +18,14 @@ package com.thoughtworks.gocd.secretmanager.vault.builders;
 
 import com.thoughtworks.gocd.secretmanager.vault.models.SecretConfig;
 
-import java.util.HashMap;
 import java.util.Map;
 
 public class VaultConfigBuilderFactory {
-    private static Map<String, VaultConfigBuilder> authMethodToConfigBuilder = new HashMap<>();
-
-    static {
-        authMethodToConfigBuilder.put(SecretConfig.TOKEN_AUTH_METHOD, new TokenVaultConfigBuilder());
-        authMethodToConfigBuilder.put(SecretConfig.APPROLE_AUTH_METHOD, new AppRoleVaultConfigBuilder());
-        authMethodToConfigBuilder.put(SecretConfig.CERT_AUTH_METHOD, new CertVaultConfigBuilder());
-    }
+    private static final Map<String, VaultConfigBuilder> authMethodToConfigBuilder = Map.of(
+            SecretConfig.TOKEN_AUTH_METHOD, new TokenVaultConfigBuilder(),
+            SecretConfig.APPROLE_AUTH_METHOD, new AppRoleVaultConfigBuilder(),
+            SecretConfig.CERT_AUTH_METHOD, new CertVaultConfigBuilder()
+    );
 
     public VaultConfigBuilder builderFor(SecretConfig secretConfig) {
         return authMethodToConfigBuilder.get(secretConfig.getAuthMethod());
